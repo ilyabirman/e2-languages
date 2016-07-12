d8d6e2817f6ccebadf40a9455b7ab17c1827e3f5
Vitali Bondar <vitaliy.bondar@gmail.com>
2016-07-12 21:57:15 +0300

    Испр. от @MustangUA
    https://github.com/ilyabirman/e2-languages/pull/2#discussion_r70493892

--- uk.php
+++ uk.php
@@ -15,7 +15,7 @@ function e2l_load_strings () {
   // installer
   'pt--install' => 'Встановлення Егеї',
   'gs--user-fixes-needed' => 'Так, треба дещо виправити.',
-  'gs--following-folders-missing' => 'Не знайдено наступні теки із дистрибутиву рушія:',
+  'gs--following-folders-missing' => 'Не знайдено вказані теки із дистрибутиву рушія:',
   'gs--could-not-create-them-automatically' => 'Створити їх автоматично не вдалося через нестачу прав. Завантажте на сервер повний дистрибутив.',
   'gs--and-reload-installer' => 'І перезавантажте встановлювач',
   'fb--begin' => 'Розпочати блог',
